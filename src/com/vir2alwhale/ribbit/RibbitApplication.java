ef4fee177dc02d46b5dae3ec086e20c7b8f3c0c2
vir2alwhale <matej.marjanovich@gmail.com>
2014-08-04 00:59:57 +0200

    Step 4, added parse.com library and test code

---
+++ src/com/vir2alwhale/ribbit/RibbitApplication.java
@@ -0,0 +1,21 @@
+package com.vir2alwhale.ribbit;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.parse.ParseAnalytics;
+
+public class RibbitApplication extends Application {
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		  Parse.initialize(this, "bFMrEz6O2WEoxdanzpYK5YG92NIfhaPlrITMPMjo", "9un2SNN6zjWrZ3rm63sYfVzidpSLHMREdRMKqb4B");
+		  
+		  ParseObject testObject = new ParseObject("TestObject");
+		  testObject.put("foo", "bar");
+		  testObject.saveInBackground();
+		}
+
+}
