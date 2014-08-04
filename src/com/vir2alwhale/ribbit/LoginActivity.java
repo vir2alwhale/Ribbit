d53563a5e0b199a6b2dba405881930be686cd63c
vir2alwhale <matej.marjanovich@gmail.com>
2014-08-04 15:23:27 +0200

    Step 7, created Login and corrected MainActivity
    used Parse.com for Login.

--- src/com/vir2alwhale/ribbit/LoginActivity.java
+++ src/com/vir2alwhale/ribbit/LoginActivity.java
@@ -1,20 +1,80 @@
 package com.vir2alwhale.ribbit;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends Activity {
 	protected TextView mSignUpTextView;
+	
+	protected EditText mUsername;
+	protected EditText mPassword;
+	protected Button mLoginButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		mUsername = (EditText) findViewById(R.id.usernameField);
+		mPassword = (EditText) findViewById(R.id.passwordField);
+		mLoginButton = (Button) findViewById(R.id.loginButton);
+		
+		mLoginButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String username = mUsername.getText().toString().trim();
+				String password = mPassword.getText().toString().trim();
+				
+				if (username.isEmpty() || password.isEmpty()) {
+					AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+					builder.setMessage(R.string.login_error_message)
+					.setTitle(R.string.login_error_title)
+					.setPositiveButton(android.R.string.ok, null);
+					
+					AlertDialog dialog = builder.create();
+					dialog.show();
+				}
+				else {
+					
+					ParseUser.logInInBackground(username, password, new LogInCallback(){
+
+						@Override
+						public void done(ParseUser user, ParseException e) {
+							if (e==null) {
+								// Success!
+								Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+								intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+								intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+								startActivity(intent);
+							} else {
+								// Something went wrong.
+								AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+								builder.setMessage(e.getMessage())
+								.setTitle(R.string.login_error_title)
+								.setPositiveButton(android.R.string.ok, null);
+								
+								AlertDialog dialog = builder.create();
+								dialog.show();
+							}
+						}
+						
+					});					
+				}
+			}
+		});
+		
 		
 		mSignUpTextView = (TextView) findViewById(R.id.signUpText);
 		mSignUpTextView.setOnClickListener(new View.OnClickListener() {
@@ -22,7 +82,7 @@ public class LoginActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
-				startActivity(intent);				
+				startActivity(intent);
 			}
 		});
 	}
--- src/com/vir2alwhale/ribbit/MainActivity.java
+++ src/com/vir2alwhale/ribbit/MainActivity.java
@@ -11,11 +11,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.parse.ParseUser;
 
 
 public class MainActivity extends Activity implements ActionBar.TabListener {
@@ -34,18 +37,26 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
      * The {@link ViewPager} that will host the section contents.
      */
     ViewPager mViewPager;
+    
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        Intent intent = new Intent(this, LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-
-        // Set up the action bar.
+        ParseUser currentUser = ParseUser.getCurrentUser();        
+        if (currentUser==null) {
+			Intent intent = new Intent(this, LoginActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+			startActivity(intent);
+		}
+        else {
+        	Log.i(TAG, currentUser.getUsername());
+        }
+        
+		// Set up the action bar.
         final ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
