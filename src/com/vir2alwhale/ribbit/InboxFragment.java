4bfca66880523566bbb960d3a8a3a646ed80f354
vir2alwhale <matej.marjanovich@gmail.com>
2014-08-13 16:32:13 +0200

    Step 1, Retrieving Messages
    First commit for “sending messages” section.

--- src/com/vir2alwhale/ribbit/InboxFragment.java
+++ src/com/vir2alwhale/ribbit/InboxFragment.java
@@ -1,17 +1,75 @@
 package com.vir2alwhale.ribbit;
 
+import java.util.List;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 public class InboxFragment extends ListFragment {
+	
+	public static final String TAG = InboxFragment.class.getSimpleName();
+	
+	protected List<ParseObject> mMessages;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
         return rootView;
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	getActivity().setProgressBarIndeterminateVisibility(true);
+    	
+    	ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(ParseConstants.CLASS_MESSAGES);
+    	query.whereEqualTo(ParseConstants.KEY_RECIPIENT_IDS, ParseUser.getCurrentUser().getObjectId());
+    	query.addDescendingOrder(ParseConstants.KEY_CREATED_AT);
+    	query.findInBackground(new FindCallback<ParseObject>() {
+			
+			@Override
+			public void done(List<ParseObject> messages, ParseException e) {
+				getActivity().setProgressBarIndeterminateVisibility(false);
+				
+				if (e==null) {
+					mMessages = messages;
+					String[] message_usernames = new String[messages.size()];
+					int i = 0;
+					for (ParseObject message : mMessages) {
+						message_usernames[i] = message.getString(ParseConstants.KEY_SENDER_NAME);
+						i++;
+					}
+					ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+							getListView().getContext(),
+							android.R.layout.simple_list_item_1,
+							message_usernames);
+					setListAdapter(adapter);
+				}
+				else {
+					Log.e(TAG, e.getMessage());
+					
+					AlertDialog.Builder builder = new AlertDialog.Builder(getListView().getContext());
+					builder.setMessage(e.getMessage())
+					.setTitle(R.string.error_title)
+					.setPositiveButton(android.R.string.ok, null);
+					AlertDialog dialog = builder.create();
+					dialog.show();
+				}
+				
+			}
+		});
     }
 }
