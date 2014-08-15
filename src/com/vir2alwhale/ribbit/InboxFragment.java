41e145a5e75de104338d09b6a118dcb504a641ab
vir2alwhale <matej.marjanovich@gmail.com>
2014-08-15 14:14:09 +0200

    Step 4, Viewing Image Messages

--- src/com/vir2alwhale/ribbit/InboxFragment.java
+++ src/com/vir2alwhale/ribbit/InboxFragment.java
@@ -2,20 +2,23 @@ package com.vir2alwhale.ribbit;
 
 import java.util.List;
 
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
-
 import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 public class InboxFragment extends ListFragment {
 	
@@ -68,5 +71,23 @@ public class InboxFragment extends ListFragment {
 				
 			}
 		});
+    }
+    
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+    	super.onListItemClick(l, v, position, id);
+    	ParseObject message = mMessages.get(position);
+    	String messageType = message.getString(ParseConstants.KEY_FILE_TYPE);
+    	ParseFile file = message.getParseFile(ParseConstants.KEY_FILE);
+    	Uri fileUri = Uri.parse(file.getUrl());
+    	
+    	if(messageType.equals(ParseConstants.TYPE_IMAGE)) {
+    		Intent intent = new Intent(getActivity(), ViewImageActivity.class);
+    		intent.setData(fileUri);
+    		startActivity(intent);
+    	}
+    	else {
+    		// Start activity with video.
+    	}
     }
 }
